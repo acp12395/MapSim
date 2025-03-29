a6f0314e9ef377583481b1919b5eb3faecc65265
acp12395 <acp12395@gmail.com>
2025-03-29 20:30:59 +0900

    Definitions in MapScreen to be used to draw the current position

--- module/MapScreen.py
+++ module/MapScreen.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 
 from .Observer import Observer
+from .GeometricCalculator import GeometricCalculator
 
 class MapScreen(Observer):
+    _geometricCalc = GeometricCalculator()
     _mapScreen = None
     _dataBase = None
     _windowHandle = None
@@ -24,3 +26,19 @@ class MapScreen(Observer):
     def _drawMapScreen(self):
         self._mapScreen = tk.Canvas(self._windowHandle, background="black")
         self._mapScreen.place(y=self._margin, x=317, relheight=1 -(2*self._margin/self._windowHandle.winfo_height()), relwidth=1 -((self._margin+332)/self._windowHandle.winfo_width()))
+
+    def drawCircle(self,x,y):
+        coord = self._mappedCoordinate(x,y)
+        self._mapScreen.create_oval(coord[0]-10,coord[1]-10,coord[0]+10,coord[1]+10,fill="white")
+    
+    def _mappedCoordinate(self,x,y):
+        return [self._mapScreen.winfo_width()//2 + x,self._mapScreen.winfo_height()//2 + y]
+    
+    def drawArrow(self,x,y,angle):
+        coord = self._mappedCoordinate(x,y)
+        dist = 8
+        front = self._geometricCalc.rotate(coord, dist,angle)
+        rearLeft = self._geometricCalc.rotate(coord, dist,angle-150)
+        rearRight = self._geometricCalc.rotate(coord, dist,angle+150)
+        vertices = [front[0],front[1],rearLeft[0],rearLeft[1],rearRight[0],rearRight[1]]
+        self._mapScreen.create_polygon(vertices,fill="red")
