fca0143d606945b354419610fb58b50536134f01
acp12395 <acp12395@gmail.com>
2025-05-28 21:36:01 +0900

    Changed node data constitution

--- module/DataBase.py
+++ module/DataBase.py
@@ -1,30 +1,32 @@
 class DataBase():
-    _data = {}
+    _crossingData = {}
 
     def __init__(self):
         pass
     
     @property
     def size(self):
-        return len(self._data)
+        return len(self._crossingData)
     
     @property
     def data(self):
-        return self._data
+        return self._crossingData
     
     def initialize(self, start):
-        #            {"CrossingName" : [complex(coordinateX,coordinateY), {"NeighborCrossingName" : [distance, "StreetName"]}]}
-        self._data = {start : [complex(0,0), dict({})]}
+        #                    {"CrossingName" : [complex(coordinateX,coordinateY), {"NeighborCrossingName" : [distance, "StreetName"]}]}
+        self._crossingData = {start : [complex(0,0), dict({})]}
+        #                        {coordinates : [[[neighborCoordinates,"NeighborCrossingName"]],[[NonAccessibleNeighborCoordinates,"NonAccessibleNeighborCrossingName"]]]}
+        self._neighborNodeData = {complex(0,0) : [[],[]]}
 
     def addRoad(self,fromCrossing,toCrossing,toCoords,distance, twoWay, roadName):
-        self._data[fromCrossing][1][toCrossing] = [distance, roadName]
+        self._crossingData[fromCrossing][1][toCrossing] = [distance, roadName]
         if twoWay:
-            self._data[toCrossing] = [toCoords, {fromCrossing : [distance,roadName]}]
+            self._crossingData[toCrossing] = [toCoords, {fromCrossing : [distance,roadName]}]
         else:
-            self._data[toCrossing] = [toCoords, dict({})]
+            self._crossingData[toCrossing] = [toCoords, dict({})]
 
     def getCoordinates(self, crossing):
         retVal = None
-        if crossing in self.data:
-            retVal = self._data[crossing][0]
+        if crossing in self._crossingData:
+            retVal = self._crossingData[crossing][0]
         return retVal
